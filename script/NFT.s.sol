a8e397f07510f07435c49b1e44944b0b75f7b2b9
rsalazvi <rsalazvi@hotmail.es>
2024-06-18 11:17:44 -0500

    NFT6

--- script/NFT.s.sol
+++ script/NFT.s.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "forge-std/Script.sol";
-import "../src/NFT.sol";
+import {Script} from "forge-std/Script.sol";
+import {NFT} from "../src/NFT.sol";
+import {console} from "forge-std/console.sol";
 
 contract NFTDeploy is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        NFT nft = new NFT("RSVILL", "RSV");
+        NFT nft = new NFT("RSVILL","RSV");
+        console.log("Staker contract deployed at: ", address(nft));
 
         vm.stopBroadcast();
     }
